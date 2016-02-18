c1c49c27371764d29553e07e2c0a187d6a1585ea
deathcap <deathcap@gmx.co.uk>
2016-02-18 00:04:17 -0800

    Port to native ES6

--- bucket.js
+++ bucket.js
@@ -1,86 +1,105 @@
+'use strict';
 
-ItemPile = require 'itempile'
-ucfirst = require 'ucfirst'
+const ItemPile = require('itempile');
+const ucfirst = require('ucfirst');
 
-module.exports = (game, opts) -> new BucketPlugin game, opts
-module.exports.pluginInfo =
-  loadAfter: ['voxel-registry', 'voxel-fluid'] # TODO: load after voxel-fluid dependants, too? post-init (other plugins might register other new fluids!)
+module.exports = (game, opts) => new BucketPlugin(game, opts);
+module.exports.pluginInfo = {
+  loadAfter: ['voxel-registry', 'voxel-fluid'] // TODO: load after voxel-fluid dependants, too? post-init (other plugins might register other new fluids!)
+};
 
-class BucketPlugin
-  constructor: (@game, @opts) ->
-    @registry = @game.plugins.get('voxel-registry') ? throw new Error('voxel-bucket requires "voxel-registry" plugin')
-    @fluidPlugin = @game.plugins.get('voxel-fluid') ? throw new Error('voxel-bucket requires "voxel-fluid" plugin')
+class BucketPlugin {
+  constructor(game, opts) {
+    this.game = game;
+    this.opts = opts;
+    this.registry = game.plugins.get('voxel-registry');
+    if (!this.registry) throw new Error('voxel-bucket requires "voxel-registry" plugin')
+    this.fluidPlugin = game.plugins.get('voxel-fluid');
+    if (!this.fluidPlugin) throw new Error('voxel-bucket requires "voxel-fluid" plugin')
 
-    @fluidBuckets = {}
+    this.fluidBuckets = {};
 
-    @opts.registerBlocks ?= true
-    @opts.registerItems ?= true
-    @opts.registerRecipes ?= true
+    if (this.opts.registerBlocks === undefined) this.opts.registerBlocks = true;
+    if (this.opts.registerItems === undefined) this.opts.registerItems = true;
+    if (this.opts.registerRecipes === undefined) this.opts.registerRecipes = true;
 
-    @enable()
+    this.enable();
+  }
 
-  enable: () ->
-    if @opts.registerItems
-      @registry.registerItem 'bucket',
-        itemTexture: 'i/bucket_empty'
-        onUse: @pickupFluid.bind(@)
-        displayName: 'Empty Bucket'
+  enable() {
+    if (this.opts.registerItems) {
+      this.registry.registerItem('bucket', {
+        itemTexture: 'i/bucket_empty',
+        onUse: this.pickupFluid.bind(this),
+        displayName: 'Empty Bucket',
         creativeTab: 'fluids'
+      });
 
-      for fluid in @fluidPlugin.getFluidNames()
-        bucketName = "bucket#{ucfirst fluid}"
-        @registry.registerItem bucketName,
-          itemTexture: "i/bucket_#{fluid}"
-          fluid: fluid
-          containerItem: 'bucket'
-          onUse: @placeFluid.bind(@, fluid)
-          displayName: "#{ucfirst fluid} Bucket"
+      for (let fluid of this.fluidPlugin.getFluidNames()) {
+        const bucketName = `bucket${ucfirst(fluid)}`;
+        this.registry.registerItem(bucketName, {
+          itemTexture: `i/bucket_${fluid}`,
+          fluid: fluid,
+          containerItem: 'bucket',
+          onUse: this.placeFluid.bind(this, fluid),
+          displayName: "${ucfirst(fluid)} Bucket",
           creativeTab: 'fluids'
+        });
 
-        @fluidBuckets[fluid] = bucketName
+        this.fluidBuckets[fluid] = bucketName;
+      }
+    }
 
-    if @opts.registerRecipes
-      @recipes = @game.plugins.get('voxel-recipes') ? throw new Error('voxel-bucket requires voxel-recipes plugin when opts.registerRecipes enabled')
+    if (this.opts.registerRecipes) {
+      this.recipes = this.game.plugins.get('voxel-recipes');
+      if (!this.recipes) throw new Error('voxel-bucket requires voxel-recipes plugin when opts.registerRecipes enabled');
 
-      @recipes.registerPositional [
+      this.recipes.registerPositional([
         ['ingotIron', undefined, 'ingotIron'],
         ['ingotIron', 'ingotIron', 'ingotIron']
-        [undefined, undefined, undefined]], # TODO: 2x3 recipe shape, not 3x3 - https://github.com/deathcap/craftingrecipes/issues/2
-          new ItemPile('bucket')
+        [undefined, undefined, undefined]], // TODO: 2x3 recipe shape, not 3x3 - https://github.com/deathcap/craftingrecipes/issues/2
+          new ItemPile('bucket'));
+    }
+  }
         
 
-  disable: () ->
-    # TODO
+  disable() {
+    // TODO
+  }
 
-  pickupFluid: (held, target) ->
-    console.log 'pickupFluid',held,target
-    return if not target
+  pickupFluid(held, target) {
+    console.log('pickupFluid',held,target);
+    if (!target) return;
 
-    name = @registry.getBlockName target.value
-    props = @registry.getBlockProps name
-    return if not props?
+    const name = this.registry.getBlockName(target.value);
+    const props = this.registry.getBlockProps(name);
+    if (props === undefined) return;
 
-    fluid = props.fluid
-    return if not fluid
+    const fluid = props.fluid;
+    if (!fluid) return;
 
-    flowing = props.flowing
-    return if flowing # can only pick up source blocks, not flowing
+    const flowing = props.flowing;
+    if (flowing) return; // can only pick up source blocks, not flowing
 
-    fluidBucket = @fluidBuckets[fluid]
-    return if not fluidBucket
+    const fluidBucket = this.fluidBuckets[fluid];
+    if (!fluidBucket) return;
 
-    # remove fluid from world, and add to returned bucket item
-    @game.setBlock target.voxel, 0
-    return new ItemPile(fluidBucket)  # replace empty bucket with filled bucket
+    // remove fluid from world, and add to returned bucket item
+    this.game.setBlock(target.voxel, 0);
+    return new ItemPile(fluidBucket);  // replace empty bucket with filled bucket
+  }
 
-  placeFluid: (fluid, held, target)  ->
-    console.log 'placeFluid',fluid,held,target
+  placeFluid(fluid, held, target) {
+    console.log('placeFluid',fluid,held,target);
 
-    return if not target
+    if (!target) return;
 
-    fluidIndex = @registry.getBlockIndex fluid
-    return if not fluidIndex?
+    const fluidIndex = this.registry.getBlockIndex(fluid);
+    if (fluidIndex === undefined) return;
 
-    # set voxel and empty bucket 
-    @game.setBlock target.adjacent, fluidIndex
-    return new ItemPile('bucket')
+    // set voxel and empty bucket
+    this.game.setBlock(target.adjacent, fluidIndex);
+    return new ItemPile('bucket');
+  }
+}
+
